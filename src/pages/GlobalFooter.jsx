d6cc7b4edde49e3997c3f75be428205b6565844e
Kunfromhun <dbqudgns9803@gmail.com>
2026-02-11 18:18:46 +0900

    feat: 푸터에 문의 이메일 추가

--- src/pages/GlobalFooter.jsx
+++ src/pages/GlobalFooter.jsx
@@ -20,7 +20,7 @@ const GlobalFooter = ({ dark }) => {
       <p style={{ fontSize: '11px', color: textColor, lineHeight: 1.8 }}>
       DeepGL (딥글) | 대표: 유병훈 | 사업자등록번호: 321-12-03154 | 통신판매업 신고번호: 123445678
         <br />
-        주소: 서울특별시 용산구 청파로57길 28-3 301호 N2실 | 연락처: 050-6617-7137
+        주소: 서울특별시 용산구 청파로57길 28-3 301호 N2실 | 연락처: 050-6617-7137 | 문의: cs@deepgl-ai.com
       </p>
       <div style={{ display: 'flex', gap: '16px' }}>
         <Link to="/privacy" style={{ fontSize: '11px', color: textColor, textDecoration: 'none' }}>개인정보 처리방침</Link>
